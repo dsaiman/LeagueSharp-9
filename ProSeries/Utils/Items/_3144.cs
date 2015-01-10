1e86847085f455e69b947b42ac3f2ed50fc8f778
legacy3 <legacy3@users.noreply.github.com>
2015-01-10 18:29:09 +0100

    added bc

--- ProSeries/Utils/Items/_3144.cs
+++ ProSeries/Utils/Items/_3144.cs
@@ -3,16 +3,16 @@ using LeagueSharp.Common;
 
 namespace ProSeries.Utils.Items
 {
-    internal class _3153 : Item
+    internal class _3144 : Item
     {
         internal override int Id
         {
-            get { return 3153; }
+            get { return 3144; }
         }
 
         internal override string Name
         {
-            get { return "Blade of the Ruined King"; }
+            get { return "Bilgewater Cutlass"; }
         }
 
         internal override float Range
@@ -31,9 +31,7 @@ namespace ProSeries.Utils.Items
 
             var targetHero = (Obj_AI_Hero) target;
 
-            if (targetHero.IsValidTarget(Range) &&
-                ProSeries.Player.Health + ProSeries.Player.GetItemDamage(targetHero, Damage.DamageItems.Botrk) <
-                ProSeries.Player.MaxHealth)
+            if (targetHero.IsValidTarget(Range))
             {
                 LeagueSharp.Common.Items.UseItem(Id, targetHero);
             }
