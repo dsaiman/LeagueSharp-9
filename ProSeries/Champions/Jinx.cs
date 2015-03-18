f1e9ed394502a7c021ddc3c474b07e5e8cf1ee7a
Robin <xrobinsong@gmail.com>
2015-03-18 15:34:48 -0700

    Jinx tested.

--- ProSeries/Champions/Jinx.cs
+++ ProSeries/Champions/Jinx.cs
@@ -23,25 +23,37 @@ namespace ProSeries.Champions
             E.SetSkillshot(0.7f, 120f, 1750f, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.6f, 140f, 1700f, false, SkillshotType.SkillshotLine);
 
-            //Spell usage.
-            ProSeries.Config.SubMenu("W").AddItem(new MenuItem("UseWCombo", "Use W on combo", true).SetValue(true));
-            ProSeries.Config.SubMenu("W").AddItem(new MenuItem("UseWHarass", "Use W on harass", true).SetValue(true));
-
-            ProSeries.Config.SubMenu("E")
-                .AddItem(new MenuItem("AutoEImmobile", "Use E on immobile targets", true).SetValue(true));
-            ProSeries.Config.SubMenu("E")
-                .AddItem(new MenuItem("AutoEClose", "Use E on closetargets", true).SetValue(true));
-
-
-            ProSeries.Config.SubMenu("R").AddItem(new MenuItem("UseR", "Use R", true).SetValue(true));
-            ProSeries.Config.SubMenu("R")
-                .AddItem(new MenuItem("MaxRDist", "MaxDistance", true).SetValue(new Slider(1500, 0, 3000)));
-
             //Drawings
             Circles.Add("W Range", W);
 
+            //Spell usage.
+            var cMenu = new Menu("Combo", "combo");
+            cMenu.AddItem(new MenuItem("combomana", "Minimum mana %")).SetValue(new Slider(5));
+            cMenu.AddItem(new MenuItem("usecombow", "Use Zap", true).SetValue(true));
+            cMenu.AddItem(new MenuItem("usecomboe", "Use Flame Chompers", true).SetValue(true));
+            cMenu.AddItem(new MenuItem("usecombor", "Use Mega Death Rocket", true).SetValue(true));
+            cMenu.AddItem(new MenuItem("usecombo", "Combo (active)")).SetValue(new KeyBind(32, KeyBindType.Press));
+            ProSeries.Config.AddSubMenu(cMenu);
+
+            var hMenu = new Menu("Harass", "harass");
+            hMenu.AddItem(new MenuItem("harassmana", "Minimum mana %")).SetValue(new Slider(55));
+            hMenu.AddItem(new MenuItem("useharassw", "Use Zap", true).SetValue(true));
+            hMenu.AddItem(new MenuItem("useharass", "Harass (active)")).SetValue(new KeyBind(67, KeyBindType.Press));
+            ProSeries.Config.AddSubMenu(hMenu);
+
+            var wMenu = new Menu("Farming", "farming");
+            wMenu.AddItem(new MenuItem("clearmana", "Minimum mana %")).SetValue(new Slider(35));
+            wMenu.AddItem(new MenuItem("useclear", "Wave/Jungle (active)")).SetValue(new KeyBind(86, KeyBindType.Press));
+            ProSeries.Config.AddSubMenu(wMenu);
+
+            var mMenu = new Menu("Misc", "misc");
+            mMenu.AddItem(new MenuItem("maxrdist", "Max R distance", true)).SetValue(new Slider(1500, 0, 3000));
+            mMenu.AddItem(new MenuItem("useeimm", "Use E on Immobile", true)).SetValue(true);
+            mMenu.AddItem(new MenuItem("useedash", "Use E on Dashing", true)).SetValue(true);
+            ProSeries.Config.AddSubMenu(mMenu);
+
             //Events
-            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.OnUpdate += Game_OnGameUpdate;
             Orbwalking.AfterAttack += OrbwalkingOnAfterAttack;
         }
 
@@ -67,42 +79,56 @@ namespace ProSeries.Champions
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            if ((ProSeries.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo &&
-                 ProSeries.Config.SubMenu("W").Item("UseWCombo", true).GetValue<bool>() ||
-                 ProSeries.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed &&
-                 ProSeries.Config.SubMenu("W").Item("UseWHarass", true).GetValue<bool>()) && W.IsReady())
+            if (ProSeries.CanCombo())
             {
-                var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Physical);
-                if (target != null && ProSeries.Orbwalker.GetTarget() == null)
+                var wtarget = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Physical);
+                if (wtarget.IsValidTarget() && W.IsReady())
                 {
-                    W.Cast(target);
+                    if (ProSeries.Config.Item("usecombow", true).GetValue<bool>())
+                        W.CastIfHitchanceEquals(wtarget, HitChance.Medium);
+                }
+
+                if (ProSeries.Config.Item("usecomboe", true).GetValue<bool>() && E.IsReady())
+                {
+                    foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget(400) && h.IsMelee()))
+                    {
+                        E.CastIfHitchanceEquals(target, HitChance.High);
+                    }
                 }
             }
 
-            if (ProSeries.Config.SubMenu("E").Item("AutoEImmobile", true).GetValue<bool>() && E.IsReady())
+            if (ProSeries.CanHarass())
+            {
+                var wtarget = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Physical);
+                if (wtarget.IsValidTarget() && W.IsReady())
+                {
+                    if (ProSeries.Config.Item("useharassw", true).GetValue<bool>())
+                        W.CastIfHitchanceEquals(wtarget, HitChance.Medium);
+                }
+            }
+
+
+            if (E.IsReady())
             {
                 foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget(E.Range)))
                 {
-                    E.CastIfHitchanceEquals(target, HitChance.Immobile);
+                    if (ProSeries.Config.Item("useeimm", true).GetValue<bool>())
+                        E.CastIfHitchanceEquals(target, HitChance.Immobile);
+
+                    if (ProSeries.Config.Item("useedash", true).GetValue<bool>())
+                        E.CastIfHitchanceEquals(target, HitChance.Dashing);
                 }
             }
 
-            if (ProSeries.Config.SubMenu("E").Item("AutoEClose", true).GetValue<bool>() && E.IsReady())
+            if (ProSeries.Config.Item("usecombor", true).GetValue<bool>())
             {
-                foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget(400) && h.IsMelee()))
-                {
-                    E.CastIfHitchanceEquals(target, HitChance.High);
-                }
-            }
-
-            if (ProSeries.Config.SubMenu("R").Item("UseR", true).GetValue<bool>())
-            {
-                var maxDistance = ProSeries.Config.SubMenu("R").Item("MaxRDist", true).GetValue<Slider>().Value;
+                var maxDistance = ProSeries.Config.Item("maxrdist", true).GetValue<Slider>().Value;
                 foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget(maxDistance)))
                 {
                     var aaDamage = Orbwalking.InAutoAttackRange(target)
                         ? ProSeries.Player.GetAutoAttackDamage(target, true)
                         : 0;
+
                     if (target.Health - aaDamage <= ProSeries.Player.GetSpellDamage(target, SpellSlot.R))
                     {
                         R.Cast(target);
